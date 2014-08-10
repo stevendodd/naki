38f5af98881f4b94e6754a1b31d39fe2bbc87916
Steven Dodd <stevendodd@outlook.com>
2014-08-10 11:41:10 +0000

    added installer for arduino toolchain setup

---
+++ arduino/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo mv /usr/share/arduino/hardware/arduino/boards.txt /usr/share/arduino/hardware/arduino/board.txt.bak
+sudo cp boards.txt /usr/share/arduino/hardware/arduino/
+sudo mv /usr/share/arduino/hardware/arduino/programmers.txt /usr/share/arduino/hardware/arduino/programmers.txt.bak
+sudo cp programmers.txt /usr/share/arduino/hardware/arduino/
+
+mkdir -p ~/sketchbook
+cp -R libraries ~/sketchbook/
+cp upload.sh ~/sketchbook/
---
+++ arduino/upload.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Burn Bootloader
+#/usr/share/arduino/hardware/tools/avrdude -C/usr/share/arduino/hardware/tools/avrdude.conf -q -patmega328 -cgpio -e -Ulock:w:0x3F:m -Uefuse:w:0x07:m -Uhfuse:w:0xD9:m -Ulfuse:w:0xFF:m
+/usr/share/arduino/hardware/tools/avrdude -C/usr/share/arduino/hardware/tools/avrdude.conf -q -patmega328 -cgpio -Uflash:w:/usr/share/arduino/hardware/arduino/bootloaders/atmega/ATmegaBOOT
+_168_atmega328.hex:i -Ulock:w:0x3F:m
+
+# Upload
+/usr/share/arduino/hardware/tools/avrdude -C/usr/share/arduino/hardware/tools/avrdude.conf -q -patmega328 -cgpio -PCOM1 -b57600 -D -Uflash:w:$1:i
