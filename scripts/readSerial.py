5f5f504dd26d1896973eb049395b5b3a96b69d50
Steven Dodd <stevendodd@outlook.com>
2014-08-18 23:43:51 +0000

    added photo functionailty

--- scripts/readSerial.py
+++ scripts/readSerial.py
@@ -2,6 +2,7 @@
 
 import serial
 import re
+from subprocess import call
 
 # Log all serial output debug=1 else debug=0 for just telemetry data
 debug = 0
@@ -10,6 +11,10 @@ debug = 0
 save = 1
 telemtryOutput = "/home/pi/hab/logs/telemetry.log"
 f = open(telemtryOutput, 'w')
+
+# take photos if photos=1
+photos=0
+photoDirectory = "/home/pi/hab/photos/"
 
 # Read a line from the serial port
 def readlineCR(port):
@@ -25,28 +30,47 @@ port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
 
 while True:
     rcv = readlineCR(port)
-    
+
     if debug==1:
       print (rcv.rstrip())
+      break
 
-    else:
-      # $$NAKI,04614,12:59:58,51.3254,-0.7731,125,25,28,7.42,6*31B5
-      log = re.search('\$\$NAKI,(\d+),(\d+):(\d+):(\d+),([-+]?\d*\.\d+|\d+),([-+]?\d*\.\d+|\d+),([-+]?\d*\.\d+|\d+),([-+]?\d*\.\d+|\d+),([-+]?\d*\.\d+|\d+),([-+]?\d*\.\d+|\d+),(\d+)\*(.*)', rcv.rstrip())
+    # $$NAKI,04614,12:59:58,51.3254,-0.7731,125,25,28,7.42,6*31B5
+    log = re.search('\$\$NAKI,(\d+),(\d+):(\d+):(\d+),([-+]?\d*\.\d+|\d+),([-+]?\d*\.\d+|\d+),([-+]?\d*\.\d+|\d+),([-+]?\d*\.\d+|\d+),([-+]?\d*\.\d+|\d+),([-+]?\d*\.\d+|\d+),(\d+)\*(.*)', rcv.rstrip())
+
+    if log:
+      messageId = log.group(1)
+      hours = log.group(2)
+      minutes = log.group(3)
+      seconds = log.group(4)
+      latitude = log.group(5)
+      longitude = log.group(6)
+      altitude = log.group(7)
+      extTemp = log.group(8)
+      intTemp = log.group(9)
+      voltage = log.group(10)
+      satellites = log.group(11)
+      checksum = log.group(12)
      
-      if log:
-        print "----------------------------------------------"
-        print log.group()
-        print "Message ID: " + log.group(1)
-        print "Time: " + log.group(2) + ":" + log.group(3) + ":" + log.group(4)  
-        print "Latitude: " + log.group(5)
-        print "Longitude: " + log.group(6)
-        print "Altitude: " + log.group(7)
-        print "External Temp: " + log.group(8)
-        print "Internal Temp: " + log.group(9)
-        print "Voltage: " + log.group(10)
-        print "Number of Satellites: " + log.group(11)
-        print "Checksum: " + log.group(12)
+      print "----------------------------------------------"
+      print log.group()
+      print "Message ID: " + messageId
+      print "Time: " + hours + ":" + minutes + ":" + seconds
+      print "Latitude: " + latitude
+      print "Longitude: " + longitude
+      print "Altitude: " + altitude
+      print "External Temp: " + extTemp
+      print "Internal Temp: " + intTemp
+      print "Voltage: " + voltage
+      print "Number of Satellites: " + satellites
+      print "Checksum: " + checksum
         
-        if save==1:
-          f.write(log.group() + "\n")
-          f.flush()
+      if save==1:
+        f.write(log.group() + "\n")
+        f.flush()
+
+      if photos==1:
+        if int(messageId)%5 == 0:
+          print "<<<<<<<<<<<<<<<<<< raspistill -ex auto -mm matrix -o " + photoDirectory + messageId + ".jpg  >>>>>>>>>>>>>>>>>>>\n"
+          call(["raspistill", "-ex", "auto", "-mm", "matrix", "-o",  photoDirectory + messageId + ".jpg"])
+
